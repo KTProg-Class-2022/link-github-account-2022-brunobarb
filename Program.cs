2b8666e8ab2efce2d051b28ec509ef8b58240503
brunobeef <bbarbosa2022@jpkeefehs.org>
2021-08-31 08:57:35 -0400

    hello

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace HelloClass
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Mr. Lesieur!");
+
+            Console.WriteLine("ryan");
+
         }
     }
 }
